60928539d036d582b3fe25b5243c77234aeb23a6
tomoneko2222 <166003396+tomoneko2222@users.noreply.github.com>
2025-03-28 00:43:25 +0900

    Update __init__.py

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,5 +1,6 @@
 from .tab1 import Tab1
 from .tab2 import Tab2
 from .tab3 import Tab3
+from .tab_gamepasses import GamePassTab
 
-__all__ = ['Tab1View', 'Tab2View', 'Tab3View']
+__all__ = ['Tab1', 'Tab2', 'Tab3', 'GamePassTab']
